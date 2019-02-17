b30e218acbd6f65e932193434c06c54041a2a435
Abhishek Kumar <abhishekisme1996@hotmail.com>
2019-02-17 16:53:18 +0530

    Updated the UI . Added buttons for searching movies and sorting them as trending,top-rated,upcoming and favourite.

--- app/src/main/java/com/recstudentportal/www/movielove/MainActivity.java
+++ app/src/main/java/com/recstudentportal/www/movielove/MainActivity.java
@@ -5,12 +5,17 @@ import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.arch.paging.PagedList;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -20,7 +25,9 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-
+    Toolbar toolbar_main_without_search,toolbar_main_with_search;
+    ImageView seach_icon,back_btn;
+    boolean pressDoubleToExitApp=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +44,41 @@ public class MainActivity extends AppCompatActivity {
               }
           });
           recyclerView.setAdapter(movieListAdapter);
+          toolbar_main_with_search=(Toolbar)findViewById(R.id.main_tool_bar_search);
+          toolbar_main_without_search=(Toolbar)findViewById(R.id.main_tool_bar);
+          seach_icon=(ImageView)findViewById(R.id.seach_icon);
+          seach_icon.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  toolbar_main_without_search.setVisibility(View.GONE);
+                  toolbar_main_with_search.setVisibility(View.VISIBLE);
+
+              }
+          });
+          back_btn=(ImageView)findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toolbar_main_with_search.setVisibility(View.GONE);
+                toolbar_main_without_search.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+    @Override
+    public void onBackPressed() {
+        if(pressDoubleToExitApp) {
+            super.onBackPressed();
+            return;
+        }
+        pressDoubleToExitApp=true;
+        toolbar_main_with_search.setVisibility(View.GONE);
+        toolbar_main_without_search.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pressDoubleToExitApp=false;
+            }
+        },2000);
     }
 }
